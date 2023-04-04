1585cfd7e7cd49211dd5a25fde26e11dce7ca59d
milan <m1lanatar7@gmail.com>
2023-04-04 21:28:11 +0300

    added js! 4.4. still a bit broken

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const characters = [
+const upperCharacters = [
   "A",
   "B",
   "C",
@@ -25,6 +25,8 @@ const characters = [
   "X",
   "Y",
   "Z",
+];
+const lowerCharacters = [
   "a",
   "b",
   "c",
@@ -51,16 +53,9 @@ const characters = [
   "x",
   "y",
   "z",
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
+];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const symbols = [
   "~",
   "`",
   "!",
@@ -89,5 +84,88 @@ const characters = [
   ">",
   ".",
   "?",
-  "/",
 ];
+
+const allCharacters = [].concat(
+  upperCharacters,
+  lowerCharacters,
+  numbers,
+  symbols
+);
+const results = [];
+
+let length = 15;
+let lengthEl = document.getElementById("length");
+
+let result1El = document.getElementById("result1");
+let result2El = document.getElementById("result2");
+
+let uppercaseEl = document.getElementById("uppercase");
+let lowercaseEl = document.getElementById("lowercase");
+let numbersEl = document.getElementById("numbers");
+let symbolsEl = document.getElementById("symbols");
+
+let incrementBtn = document.getElementById("icon-up");
+let decrementBtn = document.getElementById("icon-down");
+
+function incrementLength() {
+  if (length < 15) {
+    length += 1;
+    lengthEl.textContent = length;
+  }
+}
+
+function decrementLength() {
+  if (length > 8) {
+    length -= 1;
+    lengthEl.textContent = length;
+  }
+}
+
+function generatePasswords() {
+  renderPasswords();
+}
+
+function renderPasswords() {
+  result1El.value = "";
+  result2El.value = "";
+  const length = +lengthEl.value;
+  const hasLowercase = lowercaseEl.checked;
+  const hasUppercase = uppercaseEl.checked;
+  const hasNumbers = numbersEl.checked;
+  const hasSymbols = symbolsEl.checked;
+
+  let passwordArray = [];
+
+  if (hasLowercase) {
+    passwordArray = passwordArray.concat(lowerCharacters);
+  }
+  if (hasUppercase) {
+    passwordArray = passwordArray.concat(upperCharacters);
+  }
+  if (hasNumbers) {
+    passwordArray = passwordArray.concat(numbers);
+  }
+  if (hasSymbols) {
+    passwordArray = passwordArray.concat(symbols);
+  }
+
+  for (let i = 0; i < length; i++) {
+    let randomPassword1 = Math.floor(Math.random() * passwordArray.length);
+    let randomPassword2 = Math.floor(Math.random() * passwordArray.length);
+
+    result1El.value += passwordArray[randomPassword1];
+    result2El.value += passwordArray[randomPassword2];
+  }
+}
+
+function clearPasswords() {
+  result1El.value = "";
+  result2El.value = "";
+}
+
+function copyPassword1() {
+  password1El.select();
+  document.execCommand("copy");
+  alert("First Password copied to clipboard!");
+}
